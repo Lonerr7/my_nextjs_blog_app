926774110e9e18a1548a2c42f27cd345cacb5225
Lonerr7 <krabik2342@gmail.com>
2023-11-25 17:46:34 +0300

    21.2. Added button to open user page editor

--- app/components/Users/UserInfo.tsx
+++ app/components/Users/UserInfo.tsx
@@ -2,6 +2,8 @@ import { IUser } from '@/types/userTypes';
 import { FC } from 'react';
 import UpdateUserForm from './UpdateUserForm';
 import Avatar from './Avatar';
+import { SlPencil } from 'react-icons/sl';
+import Link from 'next/link';
 
 interface Props {
   isMyPage?: boolean;
@@ -10,34 +12,36 @@ interface Props {
 
 const UserInfo: FC<Props> = ({ isMyPage, user }) => {
   return (
-    <div>
-      <div className="py-12 px-6 bg-light-gray rounded-xl dark:bg-dark-blue">
-        <div className="mx-auto flex flex-col items-center max-w-[668px]">
-          <div className="flex items-center mb-6">
-            <Avatar customClassName="mr-4" avatarURL={user?.image} small />
-            <div>
-              <p className="text-xl font-medium	mb-2">{user?.username}</p>
-              <p className="dark:text-dark-gray">
-                {user?.job ? user.job : 'Unemployed'}
-              </p>
-            </div>
-          </div>
+    <div className="relative py-12 px-6 bg-light-gray rounded-xl dark:bg-dark-blue">
+      {isMyPage ? (
+        <Link
+          className="dark:text-white transition-opacity delay-[40ms] hover:opacity-60"
+          href="/my-page/edit"
+          title="Edit my page"
+        >
+          <SlPencil className="absolute top-5 right-5" size={20} />
+        </Link>
+      ) : null}
 
-          {user?.status ? (
-            <p className="text-center text-text-gray dark:text-dark-gray text-lg leading-[1.625rem]">
-              {user.status}
+      <div className="mx-auto flex flex-col items-center max-w-[668px]">
+        <div className="flex items-center mb-6">
+          <Avatar customClassName="mr-4" avatarURL={user?.image} small />
+          <div>
+            <p className="text-xl font-medium	mb-2">{user?.username}</p>
+            <p className="dark:text-dark-gray">
+              {user?.job ? user.job : 'Unemployed'}
             </p>
-          ) : null}
-
-          {true ? <ul className="mt-6">socials</ul> : null}
+          </div>
         </div>
+
+        {user?.status ? (
+          <p className="text-center text-text-gray dark:text-dark-gray text-lg leading-[1.625rem]">
+            {user.status}
+          </p>
+        ) : null}
+
+        {true ? <ul className="mt-6">socials</ul> : null}
       </div>
-      {/* {user ? (
-        <>
-          <p>{user?.username}</p>
-          {isMyPage ? <UpdateUserForm user={user} /> : null}
-        </>
-      ) : null} */}
     </div>
   );
 };
